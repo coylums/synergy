ba0d28aee0d10caf302b9d04d180b82a5851472e
JoshD <detwiler@gmail.com>
2014-07-29 13:13:41 -0400

    upppgrade
    aoeuaoeu

--- synergy.js
+++ synergy.js
@@ -1,7 +1,16 @@
 var synergy = 1;
+var maxSynergy = 9000;
+
+// lets collab to think outside the box and leverage modularity
+function RealTimeReactiveIncrementerFunc(int i) 
+{ 
+	if(i == maxSynergy) return i;
+	else RealTimeReactiveIncrementerFunc(synergy++); 
+}
+
 
 while(synergy) {
 
-	synergy++;
+	var synergy  = RealTimeReactiveIncrementerFunc(1);
 
 }
